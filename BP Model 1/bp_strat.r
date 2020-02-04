2146137a14c257056167ccdaa3bb2588a4abdd67
Hobbledehoy <15384813+chelyabinsk@users.noreply.github.com>
2020-02-04 23:10:42 +0000

    new model

--- BP Model 1/bp_strat.r
+++ BP Model 1/bp_strat.r
@@ -1,9 +1,17 @@
 # Script to identify set of effective rules
 
 # Pick some game
+# Find number of draws in data
+n_draws <- sqldf("select count(distinct event_id) from clean_data where win_chain=3 ")[1,1]
+n_games <- length(unique(clean_data$event_id))
+
+
 
 #for(n in 1:93){
-for(n in 1:93){
+n_corr = 0
+n_wrong = 0
+margin = 0
+for(n in 1:n_games){
 
 dd <- sqldf(paste("select event_id, event_time,
             odds,chain_n,count,win_chain from clean_data 
@@ -101,31 +109,54 @@ for(i in 1:length(dd_3[,1]) ){
     #print(c(i,dd_3[i,2]))
   }
   if(i < length(dd_3[,1])){
-    if(dd_3[i,3] - dd_3[i+1,3] > 0.2 ){
+   if(dd_3[i+1,3] - dd_3[i,3] > 0.2 ){
       is_valid = F
     }
   }
-  # if(i < length(dd_1[,1])){
-  #   if(dd_1[i,3] - dd_1[i+1,3] > 0.2 ){
-  #     is_valid = F
-  #   }
-  # }
-  # if(i < length(dd_2[,1])){
-  #   if(dd_2[i,3] - dd_2[i+1,3] > 0.2 ){
-  #     is_valid = F
-  #   }
-  # }
   
-  if(dd_3[i,3] <= 1.6 && dd_3[i,2] > 45){
+  if(!is.na(dd_1[i,3]) && !is.na(dd_2[i,3])){
+    if(abs(dd_1[i,3] - dd_2[i,3]) < 1){
+      is_valid=F
+    }
+  }
+  
+   if(i < length(dd_1[,1])){
+     if((dd_1[i+1,3] - dd_1[i,3]) > 0.4 ){
+       is_valid = F
+     }
+   }
+  if(i < length(dd_1[,1])){
+    if((dd_1[i,3] - dd_1[i+1,3]) > 0.2 ){
+      is_valid = F
+    }
+  }
+  if(i < length(dd_2[,1])){
+    if((dd_2[i+1,3] - dd_2[i,3]) > 0.4 ){
+      is_valid = F
+    }
+  }
+  if(i < length(dd_2[,1])){
+    if((dd_2[i,3] - dd_2[i+1,3]) > 0.2 ){
+      is_valid = F
+    }
+  }
+  
+  if(dd_3[i,3] <= 2 && dd_3[i,2] > 45){
     # Consider putting a bet
     break
   }
 }
 
 if(is_valid){
-  print(c(is_valid,max(dd_3$win_chain),n,dd_3[i,2],dd_3[i,3]))
+  if(max(dd_3$win_chain)==3){
+    n_corr = n_corr +1;
+    margin = margin + dd_3[i,3]
+  }else{
+    n_wrong = n_wrong + 1;
+  }
+  #print(c(is_valid,max(dd_3$win_chain),n,dd_3[i,2],dd_3[i,3]))
+  print(c(n_corr,n_wrong,margin-n_wrong,n))
 }
 }
 
-# Find number of draws in data
-n_draws <- sqldf("select count(distinct event_id) from clean_data where win_chain=3 ")[1,1]
+
